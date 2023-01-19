17aff42b532d1534df4a8d4b52f1bf2b60e39553
Michael Klein <michael@firstiwaslike.com>
2023-01-19 10:05:10 +0100

    Add nav-bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script>
   import "../app.css";
 </script>
+<nav class="bg-gray-400">
+  <ul class="container mx-auto h-10 flex items-center space-x-2">
+    <li>
+      <a href="/" class="hover:underline">Home</a>
+    </li>
+    <li>
+      <a href="/about" class="hover:underline">About</a>
+    </li>
+  </ul>
+</nav>
 <div class="container mx-auto">
   <slot />
 </div>
