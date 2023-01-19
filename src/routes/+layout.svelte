2a849ce9c7e49e233d9665a44ad8aebd5c98c9e3
Michael Klein <michael@firstiwaslike.com>
2023-01-19 10:16:49 +0100

    Style navigation based on active route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,18 @@
 <script>
   import "../app.css";
+  import { page } from '$app/stores';
 </script>
 <nav class="bg-gray-400">
   <ul class="container mx-auto h-10 flex items-center space-x-2">
     <li>
-      <a href="/" class="hover:underline">Home</a>
+      <a href="/" class="hover:underline" class:font-semibold={$page.route.id === '/'}>
+        Home
+      </a>
     </li>
     <li>
-      <a href="/about" class="hover:underline">About</a>
+      <a href="/about" class="hover:underline" class:font-semibold={$page.route.id === '/about'}>
+        About
+      </a>
     </li>
   </ul>
 </nav>
