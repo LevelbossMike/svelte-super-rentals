606873d9cdf76eecfb2122e35f8c312ab20844ea
Michael Klein <michael@firstiwaslike.com>
2023-01-19 15:37:18 +0100

    Add playwright e2e tests

---
+++ e2e/tests/example.spec.js
@@ -0,0 +1,18 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('has title', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5173');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Super Rentals/);
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'About' }).click();
+
+  await expect(page).toHaveTitle(/About/);
+
+  await page.getByRole('link', { name: 'Contact' }).click();
+
+  await expect(page).toHaveTitle(/Contact Us/);
+});
